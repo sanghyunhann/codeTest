956f4477bcb9cee5974e2111867a4ad32af54bd0
한상현 <gks7654@gmail.com>
2025-05-27 01:19:59 +0900

    직사각형 별찍기 (main에서 실행)

--- src/programmers/lv1/StarRectangle.java
+++ src/programmers/lv1/StarRectangle.java
@@ -7,11 +7,11 @@ public class StarRectangle {
     int a = sc.nextInt();
     int b = sc.nextInt();
 
-    for (int i=0; i<b; i++) {
-        for (int j=0; j<a; j++) {
-            System.out.print("*");
-        }
-    System.out.println();
-    }
+//    for (int i=0; i<b; i++) {
+//        for (int j=0; j<a; j++) {
+//            System.out.print("*");
+//        }
+//    System.out.println();
+//    }
 
 }
